951394f452550dfc414f379dc1fba6c7236a4e53
vitalikjr0 <134041707+vitalikjr0@users.noreply.github.com>
2024-04-16 14:20:38 +0300

    1.py

--- 1_lab/1.py
+++ 1_lab/1.py
@@ -1,3 +1,16 @@
 import requests
-r = requests.get('https://google.com')
-print(f"Програма запустилась з бібліотекою {requests.__name__} {requests.__version__} та отримала відповідь від сайту: {r.status_code}")
+
+def main():
+    url = 'https://itcollege.lviv.ua/'
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            print("Вміст сторінки:")
+            print(response.text)
+        else:
+            print(f"Не вдалося отримати вміст сторінки. Статус код: {response.status_code}")
+    except requests.exceptions.RequestException as e:
+        print("Помилка під час запиту:", e)
+
+if __name__ == "__main__":
+    main()
